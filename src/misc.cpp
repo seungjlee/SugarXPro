96455be1f60dc6c28cf9e29c7746db2adf1c0fa7
Marco Zerbinati <zerbinati.marco@gmail.com>
2018-12-19 02:22:17 +0100

    SugaR RC2

--- src/misc.cpp
+++ src/misc.cpp
@@ -127,7 +127,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "SugaR RC1 " << Version << setfill('0');
+  ss << "SugaR RC2 " << Version << setfill('0');
 
   if (Version.empty())
   {
